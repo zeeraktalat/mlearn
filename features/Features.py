797e2a937dbe8eb64381bca283b4e0a2848d2bae
Zeerak Waseem <zeerak.w@gmail.com>
2018-03-07 16:27:54 +0100

    create requirements

--- features/Features.py
+++ features/Features.py
@@ -11,7 +11,7 @@ class LinguisticFeatures(object):
     def __init__(self, methods: List[str] = [], cleaner: Callable = None, **kwargs):
         # Initialise variables
         self.args       = kwargs
-        self.methods    = methods
+        self.method     = methods
         self.method_map = {}
         self.features   = {}
 
@@ -20,3 +20,31 @@ class LinguisticFeatures(object):
         self.cleaner = cleaner
         self.sent    = SentimentIntensityAnalyzer()
 
+    @property
+    def methods(self):
+        return self.method_map
+
+    @methods.setter
+    def methods(self, methods):
+        for m in methods:
+            try:
+                self.method_map.update({m: getattr(self, m)})
+            except AttributeError as e:
+                print("Method {0} not found".format(m))
+                raise(e)
+
+    @property
+    def doc(self):
+        return self.document
+
+    @doc.setter
+    def doc(self, document):
+        self.document             = document
+        self.tokens, self.stopped = self.cleaner(document)
+
+    def unigrams(self) -> list:
+        """ Returns unigrams after removal of stopwords"""
+        return self.tokens
+
+    def ngrams(self, n: int) -> list:
+        return ngrams(self.tokens, n)
