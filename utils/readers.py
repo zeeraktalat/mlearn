b92e528042c53466323c8d025644712f4b6682ee
Zeerak Waseem <zeerak.w@gmail.com>
2018-04-03 16:36:09 +0100

    Make style changes

--- utils/readers.py
+++ utils/readers.py
@@ -1,9 +1,16 @@
-import pymongo, logging, os, logger
+"""Module for data reading."""
+
+import pymongo
+import logger
 from pymongo import MongoClient
-from profilehooks import profile
+from nltk import word_tokenize
+
 
 class MongoDB(object):
+    """MongoDB connector. Everything connnected to database side."""
+
     def __init__(self, hostname, port):
+        """Initialise methods."""
         # Set up logging
         self.log = logger.initialise_loggers('mongodb', '../log/db.log')
 
@@ -16,12 +23,17 @@ class MongoDB(object):
             raise(e)
 
     def __iter__(self):
-        for doc in self.retrieve_many_records(self.collection, {}, no_cursor_timeout = False):
+        """Overload __iter__ so that reusable iterators can be used.
+
+        TODO: Unfinished.
+        """
+        # TODO Unfinished
+        for record in self.retrieve_many_records(self.collection, {}, no_cursor_timeout = False):
             yield record
 
     @property
     def database(self):
-        """Obtain and set the database used. Setter accepts s sting"""
+        """Obtain and set the database used. Setter accepts a string."""
         return self.db
 
     @database.setter
@@ -30,7 +42,7 @@ class MongoDB(object):
 
     @property
     def collection(self):
-        """Obtain and set the collection used. Setter accepts s sting"""
+        """Obtain and set the collection used. Setter accepts a string."""
         return self.coll
 
     @collection.setter
@@ -39,14 +51,16 @@ class MongoDB(object):
 
     @property
     def indices(self):
-        """ Retrieve indices for a given collection
+        """Retrieve indices for a given collection.
+
         :returns indices: Dictionary of indices
         """
         return self.coll.index_information()
 
     @indices.setter
     def indices(self, indices):
-        """ Create indices
+        """Create indices.
+
         :param indices: Fields to be used as indices, see pymongo API for format.
         """
         try:
@@ -56,14 +70,16 @@ class MongoDB(object):
             raise(e)
 
     def retrieve_one_record(self, query, *args):
-        """ Retrieve or store single record
+        """Retrieve or store single record.
+
         :query: Query sought
         :*args: Other limitations or inputs to query
         """
         return self.coll.find_one(query, *args)
 
     def store_one_record(self, record, collection):
-        """ Insert a recording into collection. Only writes if _id does not exist already
+        """Insert a recording into collection. Only writes if _id does not exist already.
+
         :param record: Record to insert
         :param collection: Collection to write to
         :param *args: Additional arguments to specify
@@ -89,7 +105,8 @@ class MongoDB(object):
         return 1
 
     def update_one_record(self, _id, updates, collection, *args, **kwargs):
-        """ Updates one document in DB
+        """Update one document in DB.
+
         :param _id: ID to filter by
         :param updates: Dict containing information on which fields are updated and their values.
         :param collection: Collection to find document in.
@@ -97,7 +114,7 @@ class MongoDB(object):
         try:
             assert(isinstance(updates, dict))
         except AssertionError as e:
-            self.log.error("Input is not a dict. Type is: {0} Error:\n{1}".format(type(record), e))
+            self.log.error("Input is not a dict. Type is: {0} Error:\n{1}".format(type(updates), e))
             return False
 
         try:
@@ -108,33 +125,37 @@ class MongoDB(object):
         return True
 
     def retrieve_many_records(self, collection, query, **kwargs):
-        """Returns all records that satisfy query
+        """Return all records that satisfy query.
+
         :param query: Filtering query
         :param *args: Optional arguments
         :returns resultset: Mongo ResultSet
         """
         return self.db[collection].find(query, **kwargs)
 
-def ReadMongo(object):
-    def __init__(self, conn, dbs):
-        self.conn = conn
-        self.databses = dbs
 
-    def __iter__(self):
-        for db in self.databases:
-            self.conn.database = db
-            for coll in self.conn.get_collection_names():
-                self.conn.collection = coll
-
-                for doc in self.conn.retrieve_many_records(coll, {}, no_cursor_timeout = False):
-                    if 'Posts' in coll:
-                        if db == 'VoatDB':
-                            tokenised = word_tokenize(doc['title'].lower())
-                        else:
-                            tokenised = word_tokenize(doc['selftext'].lower() + ' ' + doc['title'].lower())
-                    else:
-                        if db == 'VoatDB':
-                            tokenised = word_tokenize(doc['text'].lower())
-                        else:
-                            tokenised = word_tokenize(doc['comment'].lower())
-                    yield tokenised
+# def ReadMongo(object):
+#     def __init__(self, conn, dbs):
+#         self.conn = conn
+#         self.databses = dbs
+#
+#     def __iter__(self):
+#         for db in self.databases:
+#             self.conn.database = db
+#             for coll in self.conn.get_collection_names():
+#                 self.conn.collection = coll
+#
+#                 for doc in self.conn.retrieve_many_records(coll, {}, no_cursor_timeout = False):
+#                     if 'Posts' in coll:
+#                         if db == 'VoatDB':
+#                             tokenised = word_tokenize(doc['title'].lower())
+#                         else:
+#                             tokenised = word_tokenize(doc['selftext'].lower() +
+#                                                       ' ' +
+#                                                       doc['title'].lower())
+#                     else:
+#                         if db == 'VoatDB':
+#                             tokenised = word_tokenize(doc['text'].lower())
+#                         else:
+#                             tokenised = word_tokenize(doc['comment'].lower())
+#                     yield tokenised
