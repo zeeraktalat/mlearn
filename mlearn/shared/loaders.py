4a05adc615b0544e4fe18cb47dbcf971bb310c6e
Zeerak Waseem <zeerakw@laptop>
2020-03-19 16:46:27 +0000

    Fix loading so that all cases now result in a dev and test set.

--- mlearn/shared/loaders.py
+++ mlearn/shared/loaders.py
@@ -4,20 +4,27 @@ from .data import GeneralDataset
 
 def _loader(args: dict):
     """Loads the dataset.
-
     :args (dict): Dict containing arguments to load dataaset.
     :returns: Loaded and splitted dataset.
     """
-
     dataset = GeneralDataset(**args)
     dataset.load('train')
 
-    if args['dev'] is not None:
+    if (args['dev'], args['test']) == (None, None):  # Only train set is given.
+        dataset.split(dataset.data, [0.8, 0.1, 0.1])
+
+    elif args['dev'] is not None and args['test'] is None:  # Dev set is given, test it not.
         dataset.load('dev')
-    if args['test'] is not None:
-        dataset.load('test')
-    if (args['dev'], args['test']) == (None, None):
         dataset.split(dataset.data, 0.8)
+
+    elif args['dev'] is None and args['test'] is not None:  # Test is given, dev is not.
+        dataset.split(dataset.data, 0.8)
+        dataset.dev_set = dataset.test
+        dataset.load('test')
+
+    else:  # Both dev and test sets are given.
+        dataset.load('dev')
+        dataset.load('test')
 
     return dataset
 
@@ -28,7 +35,6 @@ def davidson_to_binary(label: str) -> str:
     :label: Raw label as string
     :returns: label as int.
     """
-
     if label in ['0', '1']:
         return 'abuse'
     else:
@@ -68,11 +74,9 @@ def davidson(cleaners: base.Callable, preprocessor: base.Callable = None):
 
 def waseem_to_binary(label: str) -> str:
     """Turn Waseem labels into binary labels.
-
     :label: String as label.
     :returns: label
     """
-
     if label.lower() in ['sexism', 'racism', 'both']:
         return 'abuse'
     else:
@@ -137,10 +141,8 @@ def waseem_hovy(cleaners: base.Callable, preprocessor: base.Callable = None):
 
 def streamline_garcia(label: str):
     """Streamline Garcia labels with the other datasets.
-
     :returns: streamlined labels.
     """
-
     if label == 'hate':
         return 'abuse'
     else:
