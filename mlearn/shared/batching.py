fa5546dbbb5d268267d88559e5986b8e2fc08b7f
Zeerak Waseem <zeerakw@laptop>
2020-03-19 16:32:08 +0000

    Add our own batch handlers

--- mlearn/shared/batching.py
+++ mlearn/shared/batching.py
@@ -29,6 +29,27 @@ class TorchTextDefaultExtractor:
 
     def __iter__(self):
         for batch in self.data:
+            X = getattr(batch, self.df)
+            y = getattr(batch, self.lf)
+            yield (X, y)
+
+
+class BatchExtractor:
+    """A class to get the information from the batches."""
+
+    def __init__(self, datafield: str, labelfield: str, batcher: base.DataType, dataset: base.DataType,
+                 onehot: bool = True):
+        self.batcher = batcher
+        self.data = dataset
+        self.onehot = onehot
+        self.df, self.lf = datafield, labelfield
+
+    def __len__(self):
+        return len(self.batcher)
+
+    def __iter__(self):
+        for batch in self.batcher:
+            batch = self.data.encode(batch, onehot = self.onehot)
             X = torch.cat([getattr(doc, self.df) for doc in batch], dim = 0)
             y = torch.tensor([getattr(doc, self.lf) for doc in batch]).flatten()
             yield (X, y)
@@ -41,13 +62,17 @@ class Batch(object):
         self.batch_size = batch_size
         self.data = data
 
-    def create_batches(self):
-        """Go over the data and create batches."""
-        self.batches = []
-        batch = []
+    def create_batches(self, data = None):
+        """Go over the data and create batches.
+        :data (optional): Add a dataset to have batches created on."""
+
+        data = self.data if not data else data
+
+        self.batches, batch = [], []
         start_ix, end_ix = 0, self.batch_size
-        for i in range(0, len(self.data), self.batch_size):
-            batch = self.data[start_ix:end_ix]
+
+        for i in range(0, len(data), self.batch_size):
+            batch = data[start_ix:end_ix]
             start_ix, end_ix = start_ix + self.batch_size, end_ix + self.batch_size
             self.batches.append(batch)
 
