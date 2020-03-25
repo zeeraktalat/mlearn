85dacdadeb5118299ec449d6e72291e0af1ccd41
Zeerak Waseem <zeerakw@laptop>
2020-03-25 19:21:27 +0000

    Update url

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = "Mlearn",
+    name = "mlearn",
     version = "0.0.1",
     author = "Zeerak Waseem",
     author_email = "zeerak.w@gmail.com",
     decription = "A package to contain machine learning pipelines for python.",
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    url="https://github.com/zeerakw/mlearn",
+    url="git@github.com:ZeerakW/mlearn.git",
     packages = setuptools.find_packages(),
     classifiers=[
         "Programming Languge :: Python :: 3",
