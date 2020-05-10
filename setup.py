90d74b2e0ec6a166fe6b8817e4f8b571b5eeecda
Zeerak Waseem <zeerakw@laptop>
2020-05-10 22:32:34 +0100

    Try to specify pytorch archive

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setuptools.setup(
         "en_core_web_sm"
     ],
     dependency_links=[
+        "https://download.pytorch.org/whl/cu90/torch_stable.html"
         "https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.2.5/en_core_web_sm-2.2.5.tar.gz#egg=en_core_web_sm"
       ]
 )
