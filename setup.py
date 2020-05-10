037b7d484378e00a5c0400da9c77840932e6dbc5
Zeerak Waseem <zeerakw@laptop>
2020-05-10 23:03:18 +0100

    bugfix

--- setup.py
+++ setup.py
@@ -25,9 +25,8 @@ setuptools.setup(
         "spacy==2.2.2",
         "scikit-learn==0.21.3",
         "torchtestcase",
-        "en_core_web_sm @ https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.2.5/en_core_web_sm-2.2.5.tar.gz#egg=en_core_web_sm",
-        "torch==1.1.0"
+        "torch==1.1.0",
+        "en_core_web_sm @ https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.2.5/en_core_web_sm-2.2.5.tar.gz#egg=en_core_web_sm"
     ],
-    dependency_links=[
-        "https://download.pytorch.org/whl/cu90/torch_stable.html"
-        ]
+    dependency_links=["https://download.pytorch.org/whl/cu90/torch_stable.html"]
+    )
