13c3c3a95bd62d46706f336a987c7700a9740a26
Zeerak Waseem <zeerakw@laptop>
2020-06-12 05:35:56 +0100

    Add remaining batching tests and drop Batch.__getattr__

--- tests/test_batch.py
+++ tests/test_batch.py
@@ -1,6 +1,5 @@
 import os
 import torch
-import unittest
 import torchtestcase
 from mlearn.base import Field, Datapoint
 from mlearn.data.dataset import GeneralDataset
@@ -33,7 +32,7 @@ class TestBatch(torchtestcase.TorchTestCase):
         cls.dataset = 0
 
     def test_batch_sizes(self):
-        """Test that the entire dataset has been batched."""
+        """Test Batch.create_batches()."""
         batches = Batch(32, self.train)
         batches.create_batches()
         self.assertIsNotNone(batches)
@@ -41,42 +40,56 @@ class TestBatch(torchtestcase.TorchTestCase):
         self.assertEqual(counts, len(self.train))
 
     def test_batch_count(self):
-        """Test that each batch contains datapoints."""
+        """Test Batch.__len__()."""
         b = Batch(32, self.train)
         b.create_batches()
         expected = 15
         self.assertEqual(len(b), expected, msg = "The number of batches is wrong.")
 
     def test_batch_contents(self):
-        """Test that each batch contains datapoints."""
+        """Test Batch contains Datapoint."""
         b = Batch(32, self.train)
         b.create_batches()
         expected = [True for batch in b]
         output = [all(isinstance(batch_item, Datapoint) for batch_item in batch) for batch in b]
         self.assertEqual(output, expected, msg = "Not all items are datapoints.")
 
-    @unittest.skip("Test is not implemented.")
     def test_shuffle_batches(self):
-        """Test shuffling batches."""
-        pass
+        """Test Batch.shuffle_batches()."""
+        batches = Batch(32, self.train)
+        batches.create_batches()
+        expected = [batch for batch in batches]
+        result = [batch for batch in batches.shuffle_batches()]
+        self.assertCountEqual(expected, result, msg = "Shuffling batches failed: Contents in the batches have changed.")
+        self.assertFalse(expected == result, msg = "Shuffling batches failed: Batches have not been shuffled.")
 
-    @unittest.skip("Test is not implemented.")
     def test_shuffle_data(self):
-        """Test shuffling data."""
-        pass
+        """Test Batch.shuffle()."""
+        batches = Batch(32, self.train)
+        batches.create_batches()
+        expected = [item for batch in batches for item in batch]
+        result = [item for batch in batches.shuffle() for item in batch]
+        self.assertCountEqual(expected, result, msg = "Shuffling data failed: Contents of the batches have changed.")
+        self.assertFalse(expected == result, msg = "Shuffling data failed: Content has not been changed.")
 
-    @unittest.skip("Test not implemented.")
     def test_getitem__(self):
-        """Test __getitem__ function."""
-        pass
+        """Test Batch.__getitem__ function."""
+        batches = Batch(32, self.train)
+        batches.create_batches()
+        expected = next(iter(batches))
+        result = batches[0]
+        self.assertListEqual(expected, result, msg = "Batch.__getitem__ does not work.")
 
-    @unittest.skip("Test not implemented.")
-    def test_getattr__(self):
-        """Test __getattr__ function."""
-        pass
+    # def test_getattr__(self):
+    #     """Test Batch.__getattr__ function."""
+    #     batches = Batch(32, self.train)
+    #     batches.create_batches()
+    #     expected = [item.text for batch in batches for item in batch]
+    #     result = list(getattr(batches, 'text'))
+    #     self.assertListEqual(expected, result, msg = "Batch.__getattr__ does not provide the correct return value.")
 
 
-class TestBatchGenerator(unittest.TestCase):
+class TestBatchGenerator(torchtestcase.TorchTestCase):
     """Test the batchgenerator class."""
 
     @classmethod
@@ -128,14 +141,13 @@ class TestBatchGenerator(unittest.TestCase):
             self.assertIsInstance(batch[1], torch.Tensor, msg = "The type of the label element is incorrect.")
 
     def test_batch_sizes(self):
-        """Test the __len__ function of a BatchExtractor."""
+        """Test BatchExtractor.__len__."""
         batches = BatchExtractor('label', self.batches, self.dataset)
         self.assertEqual(len(batches), 8, msg = "The len operation on BatchExtractor is incorrect.")
 
     def test_onehot_encoded_batches(self):
         """Test creation of onehot encoded batches."""
         batches = BatchExtractor('label', self.batches, self.dataset, onehot = True)
-
         expected = [self.dataset.vocab_size() for batch in batches]
         output = [batch[0].size(2) for batch in batches]
         self.assertEqual(output, expected, msg = "Not all encoded items have the right length.")
@@ -143,27 +155,47 @@ class TestBatchGenerator(unittest.TestCase):
     def test_index_encoded_batches(self):
         """Test creation of index encoded batches."""
         batches = BatchExtractor('label', self.batches, self.dataset, onehot = False)
-
         expected = [self.dataset.length for _ in batches]
         output = [batch[0].size(1) for batch in batches]
         self.assertEqual(output, expected, msg = "Not all encoded items have the right length.")
 
-    @unittest.skip("Test not implemented.")
     def test_getitem__(self):
-        """Test __getitem__ function."""
-        pass
+        """Test BatchExtractor.__getitem__ function."""
+        batches = BatchExtractor('label', self.batches, self.dataset, onehot = False)
+        expected = next(iter(batches))
+        result = batches[0]
 
-    @unittest.skip("Test not implemented.")
-    def test_getattr__(self):
-        """Test __getattr__ function."""
-        pass
+        expected_ys = [exp[1] for exp in expected]
+        result_ys = [tup[1] for tup in result]
+        self.assertEqual(expected_ys[0], result_ys[0], msg = "Batch.__getitem__ labels do not match.")
 
-    @unittest.skip("Test is not implemented.")
+        expected_xs = [exp[0] for exp in expected]
+        result_xs = [tup[0] for tup in result]
+        self.assertEqual(expected_xs[0], result_xs[0], msg = "Batch.__getitem__ tensors do not match.")
+
     def test_shuffle_batches(self):
-        """Test shuffling batches."""
-        pass
+        """Test BatchExtractor.shuffle_batches()."""
+        batches = BatchExtractor('label', self.batches, self.dataset, onehot = False)
+        expected = [batch for batch in batches]
+        result = [batch for batch in batches.shuffle_batches()]
 
-    @unittest.skip("Test is not implemented.")
+        expected_ys = [exp[1].tolist() for exp in expected]
+        result_ys = [tup[1].tolist() for tup in result]
+        results = [lab == res for lab, res in zip(expected_ys, result_ys)]
+        self.assertFalse(all(results), msg = "BatchExtractor.__getitem__ batch are not shuffled.")
+
     def test_shuffle_data(self):
         """Test shuffling data."""
-        pass
+        batches = BatchExtractor('label', self.batches, self.dataset, onehot = False)
+        expected = [batch for batch in batches]
+        result = [batch for batch in batches.shuffle_batches()]
+
+        expected_ys = []
+        result_ys = []
+
+        for exp, res in zip(expected, result):
+            expected_ys.extend(exp[1].tolist())
+            result_ys.extend(res[1].tolist())
+
+        results = [lab == res for lab, res in zip(expected_ys, result_ys)]
+        self.assertFalse(all(results), msg = "BatchExtractor.__getitem__ labels are not shuffled.")
