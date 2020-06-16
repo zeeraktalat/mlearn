a9af1ec8a0cbc3648530a20deb084f071afca496
Zeerak Waseem <zeerakw@laptop>
2020-06-16 04:06:26 +0100

    Add vectorisation information.

--- mlearn/utils/pipeline.py
+++ mlearn/utils/pipeline.py
@@ -20,3 +20,51 @@ def process_and_batch(dataset: GeneralDataset, data: base.DataType, batch_size: 
     batch.create_batches()
     batches = BatchExtractor('label', batch, dataset, onehot)
     return batches
+
+
+def select_vectorizer(vectorizer: str) -> base.VectType:
+    """
+    Identify vectorizer used and return it to be used.
+
+    :param vectorizer: Vectorizer to be used.
+    :return v: Vectorizer function.
+    """
+    if not any(vec in vectorizer for vec in ['dict', 'count', 'hash', 'tfidf']):
+        print("You need to select from the options: dict, count, hash, tfidf. Defaulting to Dict.")
+        return DictVectorizer
+
+    vect = vectorizer.lower()
+    if 'dict' in vect:
+        v = DictVectorizer
+    elif 'tfidf' in vect:
+        v = TfidfVectorizer
+    elif 'hash' in vect:
+        v = HashingVectorizer
+    elif 'count' in vect:
+        v = CountVectorizer
+    setattr(v, 'fitted', False)
+
+    return v
+
+
+def vectorize(data: base.DataType, dataset: GeneralDataset, vect: base.VectType) -> base.DataType:
+    """
+    Vectorise documents.
+
+    :dataset (data.GeneralDataset): Dataset object.
+    :data (base.DataType): Dataset to vectorize.
+    :vect (base.VectType): Vectorizer to use.
+    :returns vectorized (base.DataType): Return vectorized dataset.
+    """
+    data = [getattr(doc, getattr(f, 'name')) for f in dataset.train_fields for doc in data]
+
+    if vect.fitted:
+        vectorized = vect.transform(data)
+    else:
+        vect.fit(data)
+        vectorized = vect.transform(data)
+    return vectorized
+
+
+def top_features():
+    raise NotImplementedError
