57d5561d2fe834656f2c861fdaead3658905e61e
Zeerak Waseem <zeerak.w@gmail.com>
2020-06-22 07:45:37 +0100

    Caems Bugfixes (#54)
    * fixing some bugs and mutable params
    
    * back out some path bits, lose the all-caps
    
    * use a more verbose format
    
    Co-authored-by: Megan Schneider <caelum@gmail.com>

--- mlearn/modeling/linear.py
+++ mlearn/modeling/linear.py
@@ -34,7 +34,7 @@ def train(model: ModelType, dataX: NPData, dataY: NPData,
           testX: NPData, testY: NPData,
           featurizer: Callable,
           vectorizer: str,
-          devX: NPData = [], devY: NPData = []) -> Tuple[ModelType, VectType, VectType]:
+          devX: NPData = None, devY: NPData = None) -> Tuple[ModelType, VectType, VectType]:
     """
     Train a model and return the fitted model, vectorizer, and labelencoder.
 
@@ -46,6 +46,10 @@ def train(model: ModelType, dataX: NPData, dataY: NPData,
     :param featurizer: function to transform documents into features.
     :return out_tuple: Fitted classifier, vectorizer, and labelencoder.
     """
+    if devX is None:
+        devX = []
+    if devY is None:
+        devY = []
     # Initialise things
     le = LabelEncoder()
     vect = select_vectorizer(vectorizer)
@@ -69,7 +73,7 @@ def train(model: ModelType, dataX: NPData, dataY: NPData,
 
 def evaluate_model(model: ModelType, label_encoder: VectType, vect: VectType,
                    metrics: List[str], featurizer: Callable, result_fh: str,
-                   dataX: NPData, dataY: NPData, params: dict = {}) -> dict:
+                   dataX: NPData, dataY: NPData, params: dict = None) -> dict:
     """
     Evaluate model on the data.
 
@@ -84,6 +88,8 @@ def evaluate_model(model: ModelType, label_encoder: VectType, vect: VectType,
     :param params: Parameters for the model.
     :return performance: Dictionary containing evaluations.
     """
+    if params is None:
+        params = {}
     performance = OrderedDict()
 
     # Get metric functions and generate features
