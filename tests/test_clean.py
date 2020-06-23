922b0665c5376a17c5098885cd005439e0c937ec
Zeerak Waseem <zeerakw@laptop>
2020-06-23 02:50:56 +0100

    Tests running locally

--- tests/test_clean.py
+++ tests/test_clean.py
@@ -40,7 +40,7 @@ class TestPreprocessor(unittest.TestCase):
     def test_PTB_tokenize(self):
         """Test tokenisation using PTB tags."""
 
-        expected = ['DT', 'VBZ', 'VBN', 'IN', 'NNP', 'RB', '$', 'NNP', 'IN', 'ADD']
+        expected = ['DT', 'VBZ', 'VBN', 'IN', 'NN', 'IN', '$', 'NN', 'IN', 'ADD']
         result = self.preprocess.ptb_tokenize(self.original.split(), ['lower', 'url', 'hashtag', 'username'])
         self.assertEqual(expected, result, msg = "PTB tokenisation failed.")
 
