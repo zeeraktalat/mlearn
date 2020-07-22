9c519ca004863535cf3667b3f898415471c937f0
Zeerak Waseem <zeerakw@laptop>
2020-07-22 23:30:49 +0100

    Roll back all of the 'name' stuff. Figure out another way

--- mlearn/utils/metrics.py
+++ mlearn/utils/metrics.py
@@ -7,14 +7,14 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_a
 class Metrics:
     """Metrics data object, to contain methods for computing, extracting, and evaluating different metrics."""
 
-    def __init__(self, metrics: base.List[str], display_metric: str, early_stop: str = None, avg: str = 'weighted'):
+    def __init__(self, metrics: base.List[str], display_metric: str, early_stop: str = None, avg: str = 'macro'):
         """
         Intialize metrics computation class.
 
         :metrics (base.List[str]): List of strings containing metric names.
         :display_metric (str): Metric to display in TQDM loops.
         :early_stop (str, default = None): Metric to evaluate whether to perform early stopping.
-        :avg (str, default = 'weighted'): Averaging to use for metric functions.
+        :avg (str, default = 'macro'): Averaging to use for metric functions.
         """
         self.scores, self.metrics = {}, OrderedDict()
         self.display_metric = display_metric
