5ad73e5d99311f8052b32bbf90b684043075c1ff
Zeerak Waseem <zeerakw@laptop>
2020-07-28 02:00:44 +0100

    Add possibility to set non-linearity for onehotmlp

--- mlearn/modeling/multitask.py
+++ mlearn/modeling/multitask.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 from mlearn import base
 
@@ -220,7 +221,7 @@ class OnehotMLPClassifier(nn.Module):
     """Onehot MLP MTL classifier."""
 
     def __init__(self, input_dims: base.List[int], shared_dim: int, hidden_dims: base.List[int],
-                 output_dims: base.List[int], dropout: float = 0.0, batch_first = True,
+                 output_dims: base.List[int], dropout: float = 0.0, batch_first = True, nonlinearity: str = 'tanh',
                  **kwargs) -> None:
         """
         Initialise the Multitask LSTM.
@@ -253,6 +254,99 @@ class OnehotMLPClassifier(nn.Module):
         self.inputs = {}  # Define task inputs
         for task_id, input_dim in enumerate(input_dims):
             layer = nn.Linear(input_dim, hidden_dims[0])
+            self.inputs[task_id] = layer
+
+            # Add parameters
+            self.all_parameters.append(layer.weight)
+            self.all_parameters.append(layer.bias)
+
+        self.shared = []
+        for i in range(len(hidden_dims) - 1):
+            layer = nn.Linear(hidden_dims[i], hidden_dims[i + 1])
+            self.shared.append(layer)
+
+            # Add parameters
+            self.all_parameters.append(layer.weight)
+            self.all_parameters.append(layer.bias)
+
+        self.outputs = {}
+        for task_id, _ in enumerate(hidden_dims):
+            layer = nn.Linear(hidden_dims[-1], output_dims[task_id])
+            self.outputs[task_id] = layer
+
+            # Add parameters
+            self.all_parameters.append(layer.weight)
+            self.all_parameters.append(layer.bias)
+
+        # Set the method for producing "probability" distribution.
+        self.dropout = nn.Dropout(dropout)
+        self.softmax = nn.LogSoftmax(dim = 1)
+        self.nonlinearity = torch.tanh if nonlinearity == 'tanh' else torch.relu
+
+        # Ensure that the model is deterministic (the bias term is added)
+        print(self)
+        print(list(self.all_parameters))
+
+    def forward(self, sequence, task_id) -> base.DataType:
+        """
+        Forward step in the classifier.
+
+        :param sequence: The sequence to pass through the network.
+        :param task_id: The task on which to perform forward pass.
+        :return (base.DataType): The "probability" distribution for the classes.
+        """
+        if self.batch_first:
+            sequence = sequence.transpose(0, 1)
+
+        res = self.dropout(self.inputs[task_id](sequence.float()))
+
+        for layer in self.shared:
+            res = self.dropout(layer(res))
+
+        res = res.mean(0)
+        res = self.outputs[task_id](res)
+        prob_dist = self.softmax(res)
+
+        return prob_dist
+
+
+class EmbeddingMLPClassifier(nn.Module):
+    """Embedding MLP MTL classifier."""
+
+    def __init__(self, input_dims: base.List[int], shared_dim: int, hidden_dims: base.List[int],
+                 embedding_dim: int, output_dims: base.List[int], dropout: float = 0.0, batch_first = True,
+                 **kwargs) -> None:
+        """
+        Initialise the Multitask LSTM.
+
+        :param input_dims (base.List[int]): The dimensionality of the input.
+        :param shared_dim (int): The dimensionality of the shared layer.
+        :param hidden_dims (base.List[int]): The dimensionality of the hidden dimensions for each task.
+        :param output_dims (base.List[int]): Number of classes for to predict on.
+        :param dropout (float, default = 0.0): Value of dropout layer.
+        :batch_first (boo, default = True): If input tensors have the batch dimension in the first dimensino.
+        """
+        super(EmbeddingMLPClassifier, self).__init__()
+        self.name = "onehot-mtl-mlp"
+        self.batch_first = batch_first
+        self.info = {'Input dim': ", ".join([str(it) for it in input_dims]), 'Shared dim': shared_dim,
+                     'Hidden dim': ", ".join([str(it) for it in hidden_dims]),
+                     'Output dim': ", ".join([str(it) for it in output_dims]), 'Embedding dim': embedding_dim,
+                     'Dropout': dropout, 'Model': self.name}
+
+        # Initialise the hidden dim
+        self.all_parameters = nn.ParameterList()
+
+        assert len(input_dims) == len(hidden_dims) == len(output_dims)
+
+        # Input layer (not shared) [Embedding]
+        # hidden to hidden layer (shared) [Linear]
+        # Hidden to hidden layer (not shared) [LSTM]
+        # Output layer (not shared) [Linear}
+
+        self.inputs = {}  # Define task inputs
+        for task_id, input_dim in enumerate(input_dims):
+            layer = nn.Embedding(input_dim, embedding_dim)
             self.inputs[task_id] = layer
 
             # Add parameters
