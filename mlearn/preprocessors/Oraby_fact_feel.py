87f5e8fd4156e866dbd752a942829a69a2eab726
Zeerak Waseem <zeerakw@laptop>
2020-08-02 11:01:29 +0100

    Fix style issues

--- mlearn/preprocessors/Oraby_fact_feel.py
+++ mlearn/preprocessors/Oraby_fact_feel.py
@@ -5,12 +5,13 @@ import os
 
 def process_raw(read_path: str, write_path: str):
     """
-    Read Oraby et al. (2015) in its raw format (each document is a single file. Direcotory structure are
-    splits and labels).
+    Read Oraby et al. (2015) in its raw format.
+    (each document is a single file. Direcotory structure are splits and labels).
 
     :read_path: The name of the top level directory the files are in.
     :write_path: The directory to write the files to.
     """
+
     for split_dir in os.listdir(read_path):
         outf = open(os.path.join(write_path, f"Oraby_fact_feel_{split_dir}.tsv"), 'w', encoding = 'utf-8')
         writer = csv.writer(outf, delimiter = '\t')
