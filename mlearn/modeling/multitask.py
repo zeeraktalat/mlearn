06c15b79765e161a2f691fb7289a8120d59ae400
Zeerak Waseem <zeerakw@laptop>
2020-12-17 19:19:44 +0000

    Fix bug in OnehotLSTMClassifier: add **kwargs to forward call

--- mlearn/modeling/multitask.py
+++ mlearn/modeling/multitask.py
@@ -186,7 +186,7 @@ class OnehotLSTMClassifier(nn.Module):
         print(self)
         print(list(self.all_parameters))
 
-    def forward(self, sequence, task_id) -> base.DataType:
+    def forward(self, sequence, task_id, **kwargs) -> base.DataType:
         """
         Forward step in the classifier.
 
