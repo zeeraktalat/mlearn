f2b3f853340ab4ff6cb0b49bba0eab2d0aa9ea91
Zeerak Waseem <zeerakw@laptop>
2021-02-15 21:41:33 +0000

    Add Joachim's MTL method

--- mlearn/modeling/multitask.py
+++ mlearn/modeling/multitask.py
@@ -489,3 +489,95 @@ class OnehotMLPClassifier(nn.Module):
         prob_dist = self.softmax(res)
 
         return prob_dist
+
+
+class MTMLP(nn.Module):
+
+    def __init__(self, input_dims, hidden_dims, output_dims, dropout=0.2,
+                 share_input=False, nonlinearity = 'tanh', **kwargs):
+        self.share_input = share_input
+        super(MTMLP, self).__init__()
+        self.name = 'MTMLP'
+        self.info = {'Model': self.name,
+                     'Input dim': ", ".join([str(it) for it in input_dims]), 'Hidden dim': hidden_dims,
+                     'Output dim': ", ".join([str(it) for it in output_dims]),
+                     'nonlinearity': nonlinearity, 'Dropout': dropout
+                     }
+
+        if type(hidden_dims) == int:
+            print("Warning: you passed a single integer ({}) as argument "
+                  "hidden_dims, but a list of integers specifying dimensions"
+                  "for all hidden layers is expected. The model will now have"
+                  "a single hidden layer with the dimensionality you "
+                  "specified.".format(hidden_dims))
+            hidden_dims = [hidden_dims]
+
+        self.all_parameters = nn.ParameterList()
+
+        # Define task inputs
+        self.inputs = {}
+        if share_input:
+            layer = nn.Linear(input_dims[0], hidden_dims[0])
+            self.all_parameters.append(layer.weight)
+            for task_id in range(len(input_dims)):
+                self.inputs[task_id] = layer
+        else:
+            for task_id, input_dim in enumerate(input_dims):
+                layer = nn.Linear(input_dim, hidden_dims[0])
+                self.inputs[task_id] = layer
+                self.all_parameters.append(layer.weight)
+
+        # Define shared hidden layers
+        self.shared = []
+        for i in range(len(hidden_dims) - 1):
+            layer = nn.Linear(hidden_dims[i], hidden_dims[i + 1])
+            self.shared.append(layer)
+            self.all_parameters.append(layer.weight)
+
+        # Define outputs
+        self.outputs = []
+        for output_dim in output_dims:
+            layer = nn.Linear(hidden_dims[-1], output_dim)
+            self.outputs.append(layer)
+            self.all_parameters.append(layer.weight)
+
+        # predict at lowest hidden layer, output dimensionality is number
+        # of languages (== number of tasks == number of outputs)
+        self.lang_id_output = nn.Linear(hidden_dims[0], len(output_dims))
+        self.all_parameters.append(self.lang_id_output.weight)
+
+        # Define nonlinearity and dropout (used across all hidden
+        # layers in self.forward())
+        self.tanh = nn.Tanh()
+        self.dropout = nn.Dropout(dropout)
+        # self.dropout = nn.Dropout(0.0)
+
+        # Initialize all weights
+        for weight_matrix in self.all_parameters:
+            nn.init.xavier_normal(weight_matrix)
+
+    def forward(self, x, input_task_id=0, output_all=True,
+                output_lang_id=True, **kwargs):
+        """
+        Defines a forward pass of the model. Note we don't softmax the output
+        here, this is done by the loss function
+        :param x: fixed-size input
+        :param input_task_id: which task ID to use for input
+        :param output_all: whether to return outputs for all tasks
+        :param train_mode: dropout yay or nay
+        :return: a list containing the linear class distribution
+        for every model output
+        """
+        # print(input_task_id)
+        x = x.float()
+        breakpoint()
+        dropout = self.dropout
+        x = self.inputs[input_task_id](x)
+        x = dropout(self.tanh(x))
+        for layer in self.shared:
+            x = dropout(self.tanh(layer(x)))
+
+        # output = [output(x) for output in self.outputs] if output_all else self.outputs[input_task_id](x)
+        output = self.outputs[input_task_id](x)
+        output = output.mean(1)
+        return output
